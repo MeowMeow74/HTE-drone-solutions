4b3df455b26b0d560c4ae184ad8170775620cc59
Oliver <oh2087@nyu.edu>
2025-11-15 14:43:50 -0500

    finishing template for react webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 // root component that strucutres whole webpage
 
 import { useRef } from "react";
-import Header from "./components/Header.jsx";
+import Header from "./components/Header";
+import Slideshow from "./componets/Slideshow";
+import ProductShowcase from "./components/ProductShowcase";
+import Footer from "./components/Footer";
+
