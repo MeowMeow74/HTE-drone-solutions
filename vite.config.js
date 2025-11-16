62c1d9030df73d865afb708bb802df0665452a3e
Oliver <oh2087@nyu.edu>
2025-11-15 23:28:19 -0500

    fixing github pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/HTE-solutions-react/",
+  base: "/HTE-drone-solutions/",
   plugins: [react()],
 })
