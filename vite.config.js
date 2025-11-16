5bfbf7f9a4183f2dbf7a3855f57705fc1683f9a9
Oliver <oh2087@nyu.edu>
2025-11-16 00:43:26 -0500

    edits

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base: import.meta.env.MODE === "production"
-//     ? "/HTE-drone-solutions/"
-//     : "/",
-//   plugins: [react()],
-// })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command, mode }) => {
-  return {
-    base: command === "serve"
-      ? "/"
-      : "/HTE-drone-solutions/",
-    plugins: [react()],
-  };
+export default defineConfig({
+  base: "/HTE-drone-solutions/",
+  plugins: [react()],
 });
